da2e0234249ab96a4e892bc9f380bed339fc9818
MAHBUB RAHIM <mahbubrahim926@gmail.com>
2025-04-23 14:59:00 +0600

    new cng23f

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const BASE_URL = "https://pcserver-chi.vercel.app/";
 export const S3_BASE_URL = "https://practicemania.s3.ap-south-1.amazonaws.com";
 export const API_LEVEL = "/api/v1";
 export const APP_URL = "http://localhost:3000/";
-export const LIVE_URL = "https://pcfast.vercel.app/";
+export const LIVE_URL = "https://www.practicecompanions.com/";
 // export const LIVE_URL= "http://localhost:3000/";
 export const WHISPER_API_URL = "https://api.openai.com/v1/audio/transcriptions";
 
